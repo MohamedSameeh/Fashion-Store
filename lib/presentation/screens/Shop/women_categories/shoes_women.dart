b5f6b5422b3191006648d2168aeea5cf53411375
nourhan312 <nourhanaymanghonim@gmail.com>
2024-10-12 03:02:55 +0300

    woman  collection

--- lib/presentation/screens/Shop/women_categories/shoes_women.dart
+++ lib/presentation/screens/Shop/women_categories/shoes_women.dart
@@ -1,5 +1,8 @@
-import 'package:depi_final_project/presentation/widgets/categoriesDetails.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import '../../../models/product_model.dart';
+import '../../../widgets/product_card.dart';
+import '../../../widgets/productdetails.dart';
 
 class ShoesWomen extends StatefulWidget {
   const ShoesWomen({super.key});
@@ -9,10 +12,63 @@ class ShoesWomen extends StatefulWidget {
 }
 
 class _ShoesWomenState extends State<ShoesWomen> {
+  final productsStream = FirebaseFirestore.instance
+      .collection('products')
+      .where('maincateg', isEqualTo: 'woman')
+      .where('subcateg', isEqualTo: 'Shoes')
+      .snapshots()
+      .map((snapshot) =>
+      snapshot.docs.map((doc) => Product.fromFirestore(doc)).toList());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CategoriesDetails(type: "Women Shoes ",)
+      appBar: AppBar(
+        title: Text('Women Shoes'),
+      ),
+      body: Center(
+        child: StreamBuilder<List<Product>>(
+          stream: productsStream,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error fetching products'));
+            }
+            List<Product>? products = snapshot.data;
+
+            if (products == null || products.isEmpty) {
+              return const Center(child: Text('No products found'));
+            }
+            return GridView.builder(
+              padding: const EdgeInsets.all(10),
+              itemCount: snapshot.data!.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.5,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            ProductDetailsPage(product: products[index]),
+                      ),
+                    );
+                  },
+                  child: ProductCard(product: products[index]),
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
--- lib/presentation/widgets/productdetails.dart
+++ lib/presentation/widgets/productdetails.dart
@@ -1,6 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-
 import '../models/product_model.dart';
 
 class ProductDetailsPage extends StatefulWidget {
@@ -9,7 +10,14 @@ class ProductDetailsPage extends StatefulWidget {
   const ProductDetailsPage({super.key, required this.product});
 
   @override
-  _ProductDetailsPageState createState() => _ProductDetailsPageState();
+  State<ProductDetailsPage> createState() => _ProductDetailsPageState();
+}
+
+enum CartEnum {
+  loading,
+  error,
+  existInCart,
+  notExistInCart,
 }
 
 class _ProductDetailsPageState extends State<ProductDetailsPage> {
@@ -25,8 +33,19 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
     'https://example.com/images/11.jpg',
   ];
 
+  CartEnum cartEnum = CartEnum.notExistInCart;
+
   @override
   Widget build(BuildContext context) {
+    DocumentReference cartStream =
+        FirebaseFirestore.instance.collection('cart').doc(widget.product.id);
+    final productsStream = FirebaseFirestore.instance
+        .collection('products')
+        .where('maincateg', isEqualTo: widget.product.category)
+        .where('subcateg', isEqualTo: widget.product.subcateg)
+        .snapshots()
+        .map((snapshot) =>
+            snapshot.docs.map((doc) => Product.fromFirestore(doc)).toList());
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.product.name),
@@ -127,18 +146,83 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                   ),
                   SizedBox(height: 20),
                   Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.redAccent,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-                      ),
-                      onPressed: () {
-                        // Add to cart action
-                      },
-                      child:
-                          Text('ADD TO CART', style: TextStyle(fontSize: 18)),
-                    ),
+                    child: StreamBuilder<DocumentSnapshot>(
+                        stream: cartStream.snapshots(),
+                        builder: (context,
+                            AsyncSnapshot<DocumentSnapshot> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(
+                              child: CircularProgressIndicator(
+                                color: Colors.black,
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(child: Text('Error'));
+                          }
+
+                          return !snapshot.data!.exists
+                              ? StatefulBuilder(builder: (context, setState) {
+                                  return ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.redAccent,
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 100,
+                                        vertical: 15,
+                                      ),
+                                    ),
+                                    onPressed: () async {
+                                      setState(() {
+                                        cartEnum = CartEnum.loading;
+                                      });
+
+                                      CollectionReference cartStream =
+                                          FirebaseFirestore.instance
+                                              .collection('cart');
+
+                                      await cartStream
+                                          .doc(widget.product.id)
+                                          .set({
+                                        'proname': widget.product.name,
+                                        'proprice': widget.product.price,
+                                        'prosize': selectedSize,
+                                        'procolor': selectedColor,
+                                        'proimage': widget.product.images[0],
+                                        'userid': FirebaseAuth
+                                            .instance.currentUser!.uid,
+                                        'existInCart': true,
+                                        'cartProId': widget.product.id,
+                                        'quantity': 1,
+                                      }).then((value) {
+                                        setState(() {
+                                          cartEnum = CartEnum.existInCart;
+                                        });
+                                      }).catchError((e) {
+                                        setState(() {
+                                          cartEnum = CartEnum.error;
+                                        });
+                                      });
+                                    },
+                                    child: cartEnum == CartEnum.loading
+                                        ? CircularProgressIndicator()
+                                        : Text(
+                                            'ADD TO CART',
+                                            style: TextStyle(fontSize: 18),
+                                          ),
+                                  );
+                                })
+                              : ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.redAccent,
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 100,
+                                      vertical: 15,
+                                    ),
+                                  ),
+                                  child: Text("Exist in Cart"),
+                                );
+                        }),
                   ),
                   SizedBox(height: 20),
                 ],
@@ -155,79 +239,108 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 10),
-                  Container(
-                    height: 200,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: recommendedProducts.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(right: 15),
-                          child: Container(
-                            width: 150,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Stack(
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: CachedNetworkImage(
-                                        imageUrl: recommendedProducts[index],
-                                        width: 150,
-                                        height: 150,
-                                        fit: BoxFit.cover,
-                                        placeholder: (context, url) => Center(
-                                          child: CircularProgressIndicator(),
-                                        ),
-                                        errorWidget: (context, url, error) =>
-                                            Icon(Icons.error),
-                                      ),
-                                    ),
-                                    if (index == 0)
-                                      Positioned(
-                                        top: 5,
-                                        left: 5,
-                                        child: Container(
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 5, vertical: 2),
-                                          color: Colors.redAccent,
-                                          child: Text(
-                                            '-20%',
-                                            style:
-                                                TextStyle(color: Colors.white),
+                  SizedBox(
+                    height: 250,
+                    child: StreamBuilder<List<Product>>(
+                        stream: productsStream,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(
+                              child: CircularProgressIndicator(
+                                color: Colors.black,
+                              ),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                                child: Text('Error fetching products'));
+                          }
+                          List<Product>? products = snapshot.data;
+
+                          if (products == null || products.isEmpty) {
+                            return const Center(
+                                child: Text('No products found'));
+                          }
+                          return ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(right: 15),
+                                child: SizedBox(
+                                  width: 150,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Stack(
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            child: CachedNetworkImage(
+                                              imageUrl:
+                                                  products[index].images[0],
+                                              width: 150,
+                                              height: 150,
+                                              fit: BoxFit.cover,
+                                              placeholder: (context, url) =>
+                                                  Center(
+                                                child:
+                                                    CircularProgressIndicator(),
+                                              ),
+                                              errorWidget:
+                                                  (context, url, error) =>
+                                                      Icon(Icons.error),
+                                            ),
                                           ),
+                                          if (index == 0)
+                                            Positioned(
+                                              top: 5,
+                                              left: 5,
+                                              child: Container(
+                                                padding: EdgeInsets.symmetric(
+                                                    horizontal: 5, vertical: 2),
+                                                color: Colors.redAccent,
+                                                child: Text(
+                                                  "- ${products[index].discount.toString()} %",
+                                                  style: TextStyle(
+                                                      color: Colors.white),
+                                                ),
+                                              ),
+                                            ),
+                                          Positioned(
+                                            right: 5,
+                                            bottom: 0,
+                                            child: IconButton(
+                                              icon: Icon(Icons.favorite_border),
+                                              color: Colors.white,
+                                              onPressed: () {},
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(height: 5),
+                                      Text(
+                                        products[index].name,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16),
+                                      ),
+                                      Text(
+                                        products[index].price.toString(),
+                                        style: TextStyle(
+                                          color: Colors.redAccent,
+                                          fontSize: 14,
                                         ),
                                       ),
-                                    Positioned(
-                                      right: 5,
-                                      bottom: 0,
-                                      child: IconButton(
-                                        icon: Icon(Icons.favorite_border),
-                                        color: Colors.white,
-                                        onPressed: () {},
-                                      ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                                SizedBox(height: 5),
-                                Text(
-                                  'Product Name',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16),
-                                ),
-                                Text(
-                                  '\$15.99',
-                                  style: TextStyle(
-                                      color: Colors.redAccent, fontSize: 14),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
+                              );
+                            },
+                          );
+                        }),
                   ),
                 ],
               ),
